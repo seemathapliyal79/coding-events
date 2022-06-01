b2b4c3c94612b42250af55f06f17141eefde3969
seemathapliyal79 <seema.n.thapliyal@gmail.com>
2022-06-01 13:35:01 -0700

    Edit model classes exercises complete

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,8 +4,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,12 +26,18 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent){
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:";
     }
@@ -48,4 +56,26 @@ public class EventController {
         }
         return "redirect:";
     }
+
+    @GetMapping("edit")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        // controller code will go here
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title );
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        // controller code will go here
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect:";
+    }
+
+
+
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,34 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
+
+    @NotBlank(message="Name is required")
+    @Size(min=3, max=50, message="Name must be between 3-50 characters")
     private String name;
+
+    @Size(max=500, message="Description too long")
     private String description;
-    public Event(String name, String description) {
+
+    @NotBlank(message="Email is required")
+    @Email(message="Invalid email, Try again")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id= nextId;
         nextId++;
     }
 
+    public Event(){}
     public String getName() {
         return name;
     }
@@ -28,6 +43,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
